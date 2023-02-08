84921bd433c7c40664d2e8e707515f0fc8130198
Gatsby <civin@bupt.cn>
2023-02-08 15:08:39 +0800

    init

---
+++ src/main/java/com/example/sbimgutil/SbImgUtilApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbimgutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbImgUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbImgUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbimgutil/SbImgUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbimgutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbImgUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
