f4b2f131dcb22fe059108ed5ee81c76fefe36c68
Gatsby <civin@bupt.cn>
2024-01-18 11:24:15 +0800

    UPDATE nlp任务

--- src/main/java/fun/gatsby/sbimgutil/task/NlpTask.java
+++ src/main/java/fun/gatsby/sbimgutil/task/NlpTask.java
@@ -7,6 +7,9 @@ import baidumodel.service.BaiduService;
 import cn.hutool.core.io.FileUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import fun.gatsby.lang.tuple.Tuple2;
+import fun.gatsby.lang.tuple.Tuple3;
+import fun.gatsby.lang.tuple.Tuples;
 import fun.gatsby.sbimgutil.config.AppConfig;
 import fun.gatsby.sbimgutil.schedule.ITask;
 import lombok.AllArgsConstructor;
@@ -21,24 +24,26 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.*;
-import java.util.stream.Stream;
 
 @Slf4j
 public class NlpTask extends BaseTask{
     public static final String PUNCTUATE_TRANSLATE_TEMPLATE=
-            "对古文进行解析。注意准确无误，并遵循古文的语法和语义规则。"+
-                    "输出范围: 不要其它额外信息。输出格式为json：{\"punctuated_text\":\"-\",\"chinese_text\":\"-\"}" +
-                    "指令: 对下列古文进行断句并翻译成现代中文。古文原文如下%s";
+            "对古文进行解析，注意准确无误，并遵循古文的语法和语义规则。\n"+
+            "输出范围: 不要其它额外信息。输出格式为 json：{\"punctuated_text\":\"-\",\"chinese_text\":\"-\",\"confidence\":\"-\"} \n" +
+            "指令: 对下列古文进行断句并翻译成现代中文。古文原文如下：%s";
     private final List<File> rawTextFiles;
     private final File isf;
+    private final File containerDir;
 
     File outDir;
-    int shardSize=300;
+    int maxShardSize=300;
 
-    public NlpTask(List<File> rawTextFiles, File outDir) {
-        this.rawTextFiles = rawTextFiles;
+    public NlpTask(Map.Entry<File, List<File>> entry, File outDir) {
+        this.containerDir=entry.getKey();
+        this.rawTextFiles = entry.getValue().stream().sorted(Comparator.naturalOrder()).toList();
         this.outDir = outDir;
         this.isf = new File(outDir, "intermediate_results.txt");
+        this.name="%s的%s页断句翻译任务".formatted(containerDir.getAbsolutePath(),rawTextFiles.size());
         if(!isf.exists()){
             try {
                 FileUtils.forceMkdirParent(isf);
@@ -52,22 +57,47 @@ public class NlpTask extends BaseTask{
 
     @Override
     public void doWork() throws Throwable {
-        StringBuilder sb = new StringBuilder();
-        rawTextFiles.forEach(e->{
-            String text = FileUtil.readString(e, StandardCharsets.UTF_8);
-            String replaced = text.replace("\r", "").replace("\n", "");
-            sb.append(replaced);
-        });
         var strs = FileUtil.readLines(isf, StandardCharsets.UTF_8).stream().filter(e -> !StringUtils.isEmpty(e)).toList();
-        String[] shardedRawTexts = sb.toString().split("(?<=\\G.{" + shardSize + "})");
         int startIndex=strs.size();
-        for (int i = startIndex; i < shardedRawTexts.length; i++) {
-            String text = shardedRawTexts[i];
-            NlpResult nlpResult = doNlp(text);
-            nlpResult.setIndex(i);
+        for (int i = startIndex; i < rawTextFiles.size(); i++) {
+            File pageTextFile = rawTextFiles.get(i);
+            NlpResult nlpResult = processTextFile(pageTextFile);
             FileUtils.writeStringToFile(isf, JSON.toJSONString(nlpResult) + "\n", "UTF-8", true);
-            log.debug("code:{}的{}/{}段文本处理完成", hashCode(), i + 1, shardedRawTexts.length);
+            log.info("[{}]的第[{}]页文本处理完成", name, i + 1);
         }
+    }
+
+    private NlpResult processTextFile(File pageTextFile) throws Exception {
+        String rawText = FileUtil.readString(pageTextFile, StandardCharsets.UTF_8)
+                            .replace("\r", "").replace("\n", "");
+        NlpResult nlpResult=new NlpResult();
+        nlpResult.setRawText(rawText);
+        nlpResult.setStartTime(new Date());
+        nlpResult.setFileName(pageTextFile.getName());
+        if(StringUtils.isEmpty(rawText)) {
+            nlpResult.setChineseText("");
+            nlpResult.setPunctuatedText("");
+            nlpResult.setEndTime(new Date());
+            return nlpResult;
+        }
+
+
+        StringBuilder ptsb = new StringBuilder();
+        StringBuilder ctsb = new StringBuilder();
+        var shardedRawTexts = new LinkedList<String>();
+        for (int j = 0; j < rawText.length(); j += maxShardSize) {
+            int endIndex = Math.min(j + maxShardSize, rawText.length());
+            shardedRawTexts.add(rawText.substring(j, endIndex));
+        }
+        for (String shardedRawText : shardedRawTexts) {
+            var outs = doNlp(shardedRawText);
+            ptsb.append(outs.getT1());
+            ctsb.append(outs.getT2());
+        }
+        nlpResult.punctuatedText=ptsb.toString();
+        nlpResult.chineseText=ctsb.toString();
+        nlpResult.setEndTime(new Date());
+        return nlpResult;
     }
 
     /**
@@ -75,7 +105,7 @@ public class NlpTask extends BaseTask{
      * @param rawText 原始古文
      */
     public static final BaiduService baiduService = new BaiduService("5stQ7R7IxVlDoMzSQBBGGmiS", "73QKH26Cq5SvmftOTVkjZRH53So8ysXC");
-    private NlpResult doNlp(String rawText) throws Exception{
+    private Tuple2<String, String> doNlp(String rawText) throws Exception{
         String content = String.format(PUNCTUATE_TRANSLATE_TEMPLATE, rawText);
         BaiduChatMessage msg = new BaiduChatMessage("user", content);
         ErnieBot4Param param = ErnieBot4Param.builder()
@@ -87,8 +117,6 @@ public class NlpTask extends BaseTask{
         // 构建请求参数
         // 发起请求，获取请求响应
         ErnieBot4Response ernieBot4Response;
-        NlpResult nlpResult = new NlpResult();
-        nlpResult.setStartTime(new Date());
         try {
             ernieBot4Response = baiduService.ernieBot4(param, baiduService.getToken());
         }catch (Exception e){
@@ -104,11 +132,7 @@ public class NlpTask extends BaseTask{
         JSONObject jsonObject = JSON.parseObject(replaced);
         String punctuatedText = jsonObject.getString("punctuated_text");
         String translatedText = jsonObject.getString("chinese_text");
-        nlpResult.setRawText(rawText);
-        nlpResult.setPunctuatedText(punctuatedText);
-        nlpResult.setChineseText(translatedText);
-        nlpResult.setEndTime(new Date());
-        return nlpResult;
+        return Tuples.of(punctuatedText,translatedText);
     }
 
     public static class TaskGenerator extends BaseTaskGenerator {
@@ -123,8 +147,7 @@ public class NlpTask extends BaseTask{
             for (Map.Entry<File, List<File>> entry : dirToFiles.entrySet()) {
                 File dir = entry.getKey();
                 File outDir = genOutFile(dir);
-                List<File> files = entry.getValue().stream().sorted(Comparator.naturalOrder()).toList();
-                NlpTask task = new NlpTask(files, outDir);
+                NlpTask task = new NlpTask(entry, outDir);
                 tasks.add(task);
             }
             return tasks;
@@ -154,8 +177,5 @@ class NlpResult {
     Date endTime;
     Date startTime;
 
-    /**
-     * 排序
-     */
-    Integer index;
+    String fileName;
 }
